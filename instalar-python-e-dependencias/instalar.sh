80a1b7998173fdd34134d9f770d548f743579647
Gabriel <65515076+gabriellopesdesouza2002@users.noreply.github.com>
2022-01-08 16:26:32 -0300

    Feat: Verificação se o sistema é 32 ou 64 bits, remove dependências não utilizadas, melhoria na verificação de instalação do Google Chrome.
    Verifica primeiro se o sistema é 32 ou 64 bits com uma condicional, usando uname -m;
    
    Remove dependências não utilizadas (curl, python3-venv, idle-python3.8, virtualenv, gcc, default-libmysqlclient-dev, libssl-dev).
    
    Melhoria na verificação de instalação do Google Chrome, verifica como verdadeiro se o usuário digitar (s, S, SIM, sim, y, Y)

--- instalar-python-e-dependencias/instalar.sh
+++ instalar-python-e-dependencias/instalar.sh
@@ -1,52 +1,65 @@
 #!/bin/bash
 
-tput setaf 3; echo 'Esse é o assistente de instalação para Python3, Selenium e Pyautogui'
+echo 'Esse é o assistente de instalação para fazer o bot Científicalab funcionar corretamente'
 echo
 echo 'Limpando a tela...'
 sleep 2  # Espera 2 segundos
 clear
 
-tput setaf 2; echo 'Verificando atualizações'
-echo
-sudo apt update && sudo apt upgrade -y
-echo
-echo 'Limpando a tela...'
-sleep 2  # Espera 2 segundos
-clear
+echo 'Verificando se o Sistema é 32 ou 64 bits.'
 
-tput setaf 2; echo 'Fazendo configurações para Python 3'
-echo
-sudo apt install curl git python3 python3-dev python3-venv idle-python3.8 python3-pip virtualenv gcc default-libmysqlclient-dev libssl-dev -y
-sleep 2  # Espera 2 segundos
-echo
-echo 'Limpando a tela...'
-sleep 2  # Espera 2 segundos
-clear
+arquitetura=$(uname -m)
 
-tput setaf 2; echo 'Fazendo configurações para Selenium, Pyautogui e webdriver-manager'
-sleep 2  # Espera 2 segundos
-echo
-sudo pip install selenium && pip install pyautogui && pip install webdriver-manager
-sleep 2  # Espera 2 segundos
-echo
-echo 'Limpando a tela...'
-sleep 2  # Espera 2 segundos
-clear
-
-tput setaf 2; echo 'Você gostaria de instalar o Google Chrome?  [s | n]'
-echo
-echo "Caso a resposta seja negativa, e você NÃO TIVER O CHROME INSTALADO o bot pode NÃO funcionar..."
-
-read install_chrome
-
-# shellcheck disable=SC2109
-if [ $install_chrome == 's' ] ;
+if [ $arquitetura == 'x86_64' ] ;
 then    echo
-        tput setaf 1; echo 'INSTALADO O GOOGLE CHROME'
-        sleep 3  # Espera 3 segundos
-        sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-        sudo apt install ./google-chrome-stable_current_amd64.deb
+        echo 'A sua arquitetura é 64 bits. Por isso, o processo seguirá normalmente!'
+        echo
+        echo 'Verificando atualizações...'
+        echo
+        sudo apt update && sudo apt upgrade -y
+        echo
+        echo 'Limpando a tela...'
+        sleep 2  # Espera 2 segundos
+        clear
+
+        echo 'Fazendo configurações...'
+        echo
+        sudo apt install git wget python3 python3-dev python3-tk python3-pip -y
+        sleep 2  # Espera 2 segundos
+        echo
+        echo 'Limpando a tela...'
+        sleep 2  # Espera 2 segundos
+        clear
+
+        echo 'Fazendo configurações para Selenium, Pyautogui e webdriver-manager'
+        sleep 2  # Espera 2 segundos
+        echo
+        sudo pip install selenium pyautogui webdriver-manager
+        sleep 2  # Espera 2 segundos
+        echo
+        echo 'Limpando a tela...'
+        sleep 2  # Espera 2 segundos
+        clear
+
+        echo 'Você gostaria de instalar o Google Chrome?  [s | n]'
+        echo
+        echo "Caso a resposta seja negativa, e você NÃO TIVER O CHROME INSTALADO o bot pode NÃO funcionar..."
+        echo
+        read install_chrome
+
+        if [ $install_chrome == 's' ] || [ $install_chrome == 'S' ] || [ $install_chrome == 'SIM' ] || [ $install_chrome == 'sim' ] || [ $install_chrome == 'y' ] || [ $install_chrome == 'Y' ] || [ $install_chrome == '' ] ;
+        then    echo
+                echo 'INSTALADO O GOOGLE CHROME'
+                sleep 3  # Espera 3 segundos
+                sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+                sudo apt install ./google-chrome-stable_current_amd64.deb
+        else    echo
+                echo 'SE NÃO HOUVER O CHROME INSTALADO O BOT PODE NÃO FUNCIONAR...'
+                exit
+        fi
 else    echo
-        tput setaf 3; echo 'SE NÃO HOUVER O CHROME INSTALADO O BOT PODE NÃO FUNCIONAR...'
-        read
+        echo 'A sua arquitetura é 32 bits. INFELIZMENTE O BOT NÃO SUPORTA SISTEMAS 32 bits.'
+        echo
+        clear
+        exit
 fi

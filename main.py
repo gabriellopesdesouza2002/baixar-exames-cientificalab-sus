4fc7d34b721374dbb1f223fb935b719f97d5de60
Gabriel <65515076+gabriellopesdesouza2002@users.noreply.github.com>
2022-01-15 12:25:45 -0300

    Feat: Tratamento de exceções; Verificação da arquitetura do sistema; Avisos de reinicialização do software.

--- main.py
+++ main.py
@@ -1,101 +1,149 @@
-import pyautogui
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from time import sleep
-from webdriver_manager.chrome import ChromeDriverManager
+import platform
+
+# Verificação se o sistema operacional é 32 ou 64 bits
+arquitetura = platform.machine()  # Arquitetura do OS
+
+'''
+Caso o sistema operacional seja 64 bits, o bot seguirá normalmente.
+Caso seja 32 bits, o bot mostrará uma mensagem.
+'''
+
+if '64' in arquitetura:
+    import pyautogui  # Automação de GUI
+    from selenium import webdriver  # Automação de testes de sistemas
+    from selenium.webdriver.common.by import By
+    from time import sleep
+    from webdriver_manager.chrome import ChromeDriverManager
+
+    def req():
+        try:
+            nrequest = pyautogui.prompt(text='Coloque o número da requisição:\n\n'
+                                             'Exemplo: 5784235481', title='Digite o Número da Requisição.',
+                                        default='')
+
+            if len(nrequest) >= 11 or len(nrequest) < 10 or nrequest.isalpha() or nrequest.islower() or nrequest.istitle():
+                pyautogui.alert(text=f'Você digitou algo inválido!\n'
+                                     f'O que você digitou: {nrequest}\n\n'
+                                     f'O sistema será reiniciado para que seus dados não sejam salvos.',
+                                title='SENHA!',
+                                button='Tente novamente!')
+                import sys
+                import os
+                python = sys.executable
+                os.execl(python, python, *sys.argv)
+
+            else:
+                confirma = pyautogui.confirm(text='Confira se os dados estão corretos...\n\n'
+                                                  f'Número da requisição: {nrequest}',
+                                             title='CONFIRA SEUS DADOS!',
+                                             buttons=['ESTÁ TUDO CERTO!', 'PRECISO ALTERAR...'])
+
+                if confirma == 'PRECISO ALTERAR...':
+                    pyautogui.alert(text='Por sua segurança, o sistema será reiniciado. Para não salvar suas informações.',
+                                    title='Reiniciando a aplicação', button='Reinicie')
+
+                    # Reinicia a aplicação
+                    import sys
+                    import os
+                    python = sys.executable
+                    os.execl(python, python, *sys.argv)
+                elif confirma == 'ESTÁ TUDO CERTO!':
+                    return nrequest
+        except TypeError:  # Caso o user clique em "Cancel", retornará None e vai parar o aplicativo.
+            pyautogui.alert(text='Finalizando a aplicação... ', title='Finalizando a aplicação', button='OK')
+            exit()
+        except Exception as description:
+            pyautogui.alert(text='Algo deu errado...\n'
+                                 f'Mostre isso ao Desenvolvedor: {description}',
+                            title='Erro inesperado!',
+                            button='Sair')
 
 
-def req():
-    nrequest = pyautogui.prompt(text='Coloque o número da requisição:\n\n'
-                                    'Exemplo: 5784235481', title='Digite o Número da Requisição.',
-                               default='')
-
-    if len(nrequest) >= 11 or len(nrequest) < 10 or nrequest.isalpha() or nrequest.islower() or nrequest.istitle():
-        pyautogui.alert(text=f'Você digitou algo que não é válido!', title='NÚMERO DA REQUISIÇÃO!',
-                        button='Tente novamente!')
-        import sys
-        import os
-        python = sys.executable
-        os.execl(python, python, *sys.argv)
-    else:
-        confirma = pyautogui.confirm(text='Confira se os dados estão corretos...\n\n'
-                                          f'Número da requisição: {nrequest}',
-                                     title='CONFIRA SEUS DADOS!',
-                                     buttons=['ESTÁ TUDO CERTO!', 'PRECISO ALTERAR...'])
-
-        if confirma == 'PRECISO ALTERAR...':
-            import sys
-            import os
-            python = sys.executable
-            os.execl(python, python, *sys.argv)
-        elif confirma == 'ESTÁ TUDO CERTO!':
-            return nrequest
+    nrequest = req()
 
 
-nrequest = req()
+    def passwd():
+        try:
+            password = pyautogui.password(text='Digite a senha do para acessar os resultados de exames.\n'
+                                               'Exemplo: 159732',
+                                          title='Digite Sua Senha.',
+                                          default='', mask='')
+
+            if len(password) >= 13 or len(password) < 4 or password.isalpha() or password.islower() or password.istitle():
+                pyautogui.alert(text=f'Você digitou algo inválido!\n'
+                                     f'O que você digitou: {password}\n\n'
+                                     f'O sistema será reiniciado para que seus dados não sejam salvos.',
+                                title='SENHA!',
+                                button='Tente novamente!')
+                import sys
+                import os
+                python = sys.executable
+                os.execl(python, python, *sys.argv)
+
+            confirma = pyautogui.confirm(text=f'Confira agora seus dados... \n'
+                                              f'A senha que você digitou, tem {len(password)} caracteres.\n\n'
+                                              f'Senha: {password}', title='CONFIRA SEUS DADOS!',
+                                         buttons=['ESTÁ TUDO CERTO!', 'PRECISO ALTERAR...'])
+            if confirma == 'PRECISO ALTERAR...':
+                pyautogui.alert(text='Por sua segurança, o sistema será reiniciado. Para não salvar suas informações.',
+                                title='Reiniciando a aplicação', button='Reinicie')
+
+                # Reinicia a aplicação
+                import sys
+                import os
+                python = sys.executable
+                os.execl(python, python, *sys.argv)
+            else:
+                return password
+        except TypeError:
+            pyautogui.alert(text='Finalizando a aplicação...', title='Finalizando a aplicação', button='OK')
+            exit()
+        except Exception as description:
+            pyautogui.alert(text='Algo deu errado...\n'
+                                 f'Mostre isso ao Desenvolvedor: {description}',
+                            title='Erro inesperado!',
+                            button='Sair')
 
 
-def passwd():
-    password = pyautogui.password(text='Digite a senha do para acessar os resultados de exames.\n'
-                                       'Exemplo: 159732', title='Digite Sua Senha.', default='', mask='')
+    passw = passwd()
 
-    if len(password) >= 7 or len(password) < 6 or password.isalpha() or password.islower() or password.istitle():
-        pyautogui.alert(text=f'Você digitou algo que não é válido!', title='SENHA!',
-                        button='Tente novamente!')
-        import sys
-        import os
-        python = sys.executable
-        os.execl(python, python, *sys.argv)
-
-    confirma = pyautogui.confirm(text='Confira agora seus dados... \n'
-                                      f'Senha: {password}', title='CONFIRA SEUS DADOS!',
-                                 buttons=['ESTÁ TUDO CERTO!', 'PRECISO ALTERAR...'])
-    if confirma == 'PRECISO ALTERAR...':
-        import sys
-        import os
-        python = sys.executable
-        os.execl(python, python, *sys.argv)
-    else:
-        return password
-
-
-passw = passwd()
-
-chrome = webdriver.Chrome(ChromeDriverManager().install())
-chrome.maximize_window()
-chrome.get('https://tmlablaudos.cientificalab.com.br/laudos/#')
-chrome.find_element(By.XPATH, '//*[@id="ztmFormLogin"]/div/button/span[1]').click()
-chrome.find_element(By.XPATH, '//*[@id="ztmFormLogin"]/div/div/ul/li[2]/a/span[2]').click()
-chrome.find_element(By.XPATH, '//*[@id="ztmLogin"]').send_keys(nrequest)
-chrome.find_element(By.XPATH, '//*[@id="ztmSenha"]').send_keys(passw)
-chrome.find_element(By.XPATH, '//*[@id="ztmEntrar"]').click()
-
-sleep(1)
-chrome.find_element(By.XPATH, '//*[@id="accordion"]/div/div[1]/div[1]/div').click()
-sleep(6)
-
-download_exames = pyautogui.confirm(text='Você gostaria de baixar o(s) exame(s)?',
-                                    title='Baixar?',
-                                    buttons=['Sim', 'Não'])
-
-if download_exames == 'Sim':
-    pyautogui.alert(text='ATENÇÃO: NÃO UTILIZE SEU TECLADO, ATÉ QUE O PRÓXIMO AVISO SEJA MOSTRADO!',
-                    title='ALERTA!',
-                    button='TUDO BEM...')
-
-    chrome.find_element(By.XPATH, '//*[@id="btnImprimeReq"]/span').click()
+    chrome = webdriver.Chrome(ChromeDriverManager().install())
     chrome.maximize_window()
-    sleep(12)
+    chrome.get('https://tmlablaudos.cientificalab.com.br/laudos/#')
+    chrome.find_element(By.XPATH, '//*[@id="ztmFormLogin"]/div/button/span[1]').click()
+    chrome.find_element(By.XPATH, '//*[@id="ztmFormLogin"]/div/div/ul/li[2]/a/span[2]').click()
+    chrome.find_element(By.XPATH, '//*[@id="ztmLogin"]').send_keys(nrequest)
+    chrome.find_element(By.XPATH, '//*[@id="ztmSenha"]').send_keys(passw)
+    chrome.find_element(By.XPATH, '//*[@id="ztmEntrar"]').click()
 
-    pyautogui.hotkey('ctrl', 's')
-    sleep(5)
-    pyautogui.hotkey('backspace')
-    pyautogui.alert(text=f'Meu trabalho acaba por aqui, você pode baixar o arquivo, com o nome que desejar e '
-                         f'no local que desejar também!\n\n'
-                         f'LEMBRE-SE DE COLOCAR O .pdf NO FINAL DO NOME DO ARQUIVO 😉',
-                    title='MEU TRABALHO ACABOU',
-                    button='Tudo bem, vou escolher um local e um nome para o arquivo...')
+    sleep(1)
+    chrome.find_element(By.XPATH, '//*[@id="accordion"]/div/div[1]/div[1]/div').click()
+    sleep(6)
+
+    download_exames = pyautogui.confirm(text='Você gostaria de baixar o(s) exame(s)?',
+                                        title='Baixar?',
+                                        buttons=['Sim', 'Não'])
+
+    if download_exames == 'Sim':
+        pyautogui.alert(text='ATENÇÃO: NÃO UTILIZE SEU TECLADO, ATÉ QUE O PRÓXIMO AVISO SEJA MOSTRADO!',
+                        title='ALERTA!',
+                        button='TUDO BEM...')
+
+        chrome.find_element(By.XPATH, '//*[@id="btnImprimeReq"]/span').click()
+        chrome.maximize_window()
+        sleep(12)
+
+        pyautogui.hotkey('ctrl', 's')
+        sleep(5)
+        pyautogui.hotkey('backspace')
+        pyautogui.alert(text=f'Meu trabalho acaba por aqui, você pode baixar o arquivo, com o nome que desejar e '
+                             f'no local que desejar também!\n\n'
+                             f'LEMBRE-SE DE COLOCAR O .pdf NO FINAL DO NOME DO ARQUIVO 😉',
+                        title='MEU TRABALHO ACABOU',
+                        button='Tudo bem, vou escolher um local e um nome para o arquivo...')
+    else:
+        pyautogui.alert(text=f'Meu trabalho acaba por aqui, você pode ver o(s) exame(s), ou baixar manualmente...!',
+                        title='MEU TRABALHO POR AQUI ACABOU!',
+                        button='OK...')
 else:
-    pyautogui.alert(text=f'Meu trabalho acaba por aqui, você pode ver o(s) exame(s), ou baixar manualmente...!',
-                    title='MEU TRABALHO POR AQUI ACABOU!',
-                    button='OK...')
+    print('Infelizmente o bot ainda não suporta seu sistema operacional...')
